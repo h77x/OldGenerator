889ae15e6469260ebd6594bd127cef462c478f82
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 20:59:44 +0100

    Allow legacy population access to 256-block 1.2.5 worlds

--- src/main/java/ca/spottedleaf/oldgenerator/world/WorldBlockAccess.java
+++ src/main/java/ca/spottedleaf/oldgenerator/world/WorldBlockAccess.java
@@ -7,85 +7,33 @@ import org.bukkit.block.BlockState;
 import org.bukkit.block.data.BlockData;
 
 public final class WorldBlockAccess implements BlockAccess {
-
     public final World world;
+    private final int minHeight;
+    private final int maxHeight;
 
     public WorldBlockAccess(final World world) {
+        this(world, 0, 127);
+    }
+
+    public WorldBlockAccess(final World world, final int minHeight, final int maxHeight) {
         this.world = world;
+        this.minHeight = minHeight;
+        this.maxHeight = maxHeight;
     }
 
-    @Override
-    public boolean isLoaded(final int chunkX, final int chunkZ) {
-        return this.world.isChunkLoaded(chunkX, chunkZ);
-    }
-
-    @Override
-    public Material getType(final int x, final int y, final int z) {
-        return this.world.getBlockAt(x, y, z).getType();
-    }
-
-    @Override
-    public void setType(final int x, final int y, final int z, final Material material) {
-        this.world.getBlockAt(x, y, z).setType(material);
-    }
-
-    @Override
-    public void setType(final int x, final int y, final int z, final Material material, final boolean applyPhysics) {
-        this.world.getBlockAt(x, y, z).setType(material, applyPhysics);
-    }
-
-    @Override
-    public BlockData getBlockData(final int x, final int y, final int z) {
-        return this.world.getBlockAt(x, y, z).getBlockData();
-    }
-
-    @Override
-    public void setBlockData(final int x, final int y, final int z, final BlockData data) {
-        this.world.getBlockAt(x, y, z).setBlockData(data);
-    }
-
-    @Override
-    public void setBlockData(final int x, final int y, final int z, final BlockData data, final boolean applyPhysics) {
-        this.world.getBlockAt(x, y, z).setBlockData(data, applyPhysics);
-    }
-
-    @Override
-    public BlockState getBlockState(final int x, final int y, final int z) {
-        return this.world.getBlockAt(x, y, z).getState();
-    }
-
-    @Override
-    public byte getLightFromSky(final int x, final int y, final int z) {
-        return this.world.getBlockAt(x, y, z).getLightFromSky();
-    }
-
-    @Override
-    public byte getLightFromBlocks(final int x, final int y, final int z) {
-        return this.world.getBlockAt(x, y, z).getLightFromBlocks();
-    }
-
-    @Override
-    public byte getLightLevel(final int x, final int y, final int z) {
-        return this.world.getBlockAt(x, y, z).getLightLevel();
-    }
-
-    @Override
-    public int getHighestBlockYAt(final int x, final int z) {
-        return this.world.getHighestBlockYAt(x, z);
-    }
-
-    @Override
-    public int getHighestBlockYAt(final int x, final int z, final HeightMap heightMap) {
-        return this.world.getHighestBlockYAt(x, z, heightMap);
-    }
-
-    @Override
-    public int getMinHeight() {
-        return 0;
-    }
-
-    @Override
-    public int getMaxHeight() {
-        return 127;
-    }
+    @Override public boolean isLoaded(final int chunkX, final int chunkZ) { return this.world.isChunkLoaded(chunkX, chunkZ); }
+    @Override public Material getType(final int x, final int y, final int z) { return this.world.getBlockAt(x,y,z).getType(); }
+    @Override public void setType(final int x, final int y, final int z, final Material material) { this.world.getBlockAt(x,y,z).setType(material); }
+    @Override public void setType(final int x, final int y, final int z, final Material material, final boolean physics) { this.world.getBlockAt(x,y,z).setType(material,physics); }
+    @Override public BlockData getBlockData(final int x, final int y, final int z) { return this.world.getBlockAt(x,y,z).getBlockData(); }
+    @Override public void setBlockData(final int x, final int y, final int z, final BlockData data) { this.world.getBlockAt(x,y,z).setBlockData(data); }
+    @Override public void setBlockData(final int x, final int y, final int z, final BlockData data, final boolean physics) { this.world.getBlockAt(x,y,z).setBlockData(data,physics); }
+    @Override public BlockState getBlockState(final int x, final int y, final int z) { return this.world.getBlockAt(x,y,z).getState(); }
+    @Override public byte getLightFromSky(final int x, final int y, final int z) { return this.world.getBlockAt(x,y,z).getLightFromSky(); }
+    @Override public byte getLightFromBlocks(final int x, final int y, final int z) { return this.world.getBlockAt(x,y,z).getLightFromBlocks(); }
+    @Override public byte getLightLevel(final int x, final int y, final int z) { return this.world.getBlockAt(x,y,z).getLightLevel(); }
+    @Override public int getHighestBlockYAt(final int x, final int z) { return this.world.getHighestBlockYAt(x,z); }
+    @Override public int getHighestBlockYAt(final int x, final int z, final HeightMap map) { return this.world.getHighestBlockYAt(x,z,map); }
+    @Override public int getMinHeight() { return this.minHeight; }
+    @Override public int getMaxHeight() { return this.maxHeight; }
 }
