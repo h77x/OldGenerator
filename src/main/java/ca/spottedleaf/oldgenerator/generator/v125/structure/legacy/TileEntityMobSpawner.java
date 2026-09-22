8854d053a45815fa5b2ed8e0d0453b1f639e1166
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 21:53:06 +0100

    gen: add legacy 1.2.5 TileEntityMobSpawner.java compatibility

---
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/TileEntityMobSpawner.java
@@ -0,0 +1,2 @@
+package ca.spottedleaf.oldgenerator.generator.v125.structure.legacy;
+public final class TileEntityMobSpawner { private String mobID="Pig"; public String getMobID(){return mobID;} public void setMobID(String id){mobID=id;} }
