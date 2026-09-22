ff79dc9a77ec08baa5ee6174c50aa282767a3f76
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 22:18:47 +0100

    gen: retain vanilla spawner tile state during structure generation

--- src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/TileEntityMobSpawner.java
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/TileEntityMobSpawner.java
@@ -1,2 +1,21 @@
 package ca.spottedleaf.oldgenerator.generator.v125.structure.legacy;
-public final class TileEntityMobSpawner { private String mobID="Pig"; public String getMobID(){return mobID;} public void setMobID(String id){mobID=id;} }
+
+public final class TileEntityMobSpawner {
+    private final World world;
+    private final int x, y, z;
+    private String mobID = "Pig";
+
+    public TileEntityMobSpawner(final World world, final int x, final int y, final int z) {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public String getMobID() { return mobID; }
+
+    public void setMobID(final String id) {
+        mobID = id;
+        world.syncSpawner(x, y, z, mobID);
+    }
+}
