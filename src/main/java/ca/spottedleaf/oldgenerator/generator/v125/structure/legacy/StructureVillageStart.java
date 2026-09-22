a57f81ea45cf2aa9ffd1be43da42792c1b7ca955
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 22:05:36 +0100

    fix: expose vanilla StructureVillageStart to structure adapter

--- src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/StructureVillageStart.java
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/StructureVillageStart.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
 
-class StructureVillageStart extends StructureStart
+public class StructureVillageStart extends StructureStart
 {
     /** well ... thats what it does */
     private boolean hasMoreThanTwoComponents = false;
