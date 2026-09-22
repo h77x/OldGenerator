b395673fa1961255a78218c48bbff59fcb706c8a
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 22:12:38 +0100

    gen: add vanilla diamond and redstone block IDs

--- src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/Block.java
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/Block.java
@@ -50,6 +50,8 @@ public final class Block {
     public static final Block sponge = b(19, Material.SPONGE);
     public static final Block glass = b(20, Material.GLASS);
     public static final Block oreLapis = b(21, Material.LAPIS_ORE);
+    public static final Block oreDiamond = b(56, Material.DIAMOND_ORE);
+    public static final Block oreRedstone = b(73, Material.REDSTONE_ORE);
     public static final Block blockLapis = b(22, Material.LAPIS_BLOCK);
     public static final Block sandStone = b(24, Material.SANDSTONE);
     public static final Block bed = b(26, Material.RED_BED);
