133ae68001ab4246302134fedea9737e9c0fa639
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 21:55:17 +0100

    gen: port vanilla 1.2.5 ComponentVillageRoadPiece

---
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/ComponentVillageRoadPiece.java
@@ -0,0 +1,9 @@
+package ca.spottedleaf.oldgenerator.generator.v125.structure.legacy;
+
+public abstract class ComponentVillageRoadPiece extends ComponentVillage
+{
+    protected ComponentVillageRoadPiece(int par1)
+    {
+        super(par1);
+    }
+}
