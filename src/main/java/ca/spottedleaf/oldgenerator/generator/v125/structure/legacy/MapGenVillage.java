92c1294d8c5c24c620a210011bab15c42f4eb014
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 21:57:58 +0100

    gen: add vanilla village biome list compatibility

---
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/MapGenVillage.java
@@ -0,0 +1,8 @@
+package ca.spottedleaf.oldgenerator.generator.v125.structure.legacy;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class MapGenVillage {
+    public static final List villageSpawnBiomes = Arrays.asList(BiomeGenBase.plains, BiomeGenBase.desert);
+}
