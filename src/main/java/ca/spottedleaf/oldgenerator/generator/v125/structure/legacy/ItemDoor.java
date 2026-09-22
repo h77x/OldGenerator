b7955bd823a1aad446b19197a075e65a99df0683
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 22:04:16 +0100

    fix: correct legacy door compatibility syntax

--- src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/ItemDoor.java
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/ItemDoor.java
@@ -1,8 +1,12 @@
 package ca.spottedleaf.oldgenerator.generator.v125.structure.legacy;
+
 public final class ItemDoor {
-    public static void placeDoorBlock(World world,int x,int y,int z,int meta,Block block){
-        world.setBlockAndMetadata(x,y,z,block.blockID,meta);
-        world.setBlockAndMetadata(x,y+1,z,block.blockID,8|(meta&1));
-        public static void placeDoorBlock(World world,int x,int y,int z,EnumDoor door){ placeDoorBlock(world,x,y,z,0,door==EnumDoor.IRON?Block.doorSteel:Block.doorWood); }
-}
+    public static void placeDoorBlock(World world, int x, int y, int z, int meta, Block block) {
+        world.setBlockAndMetadata(x, y, z, block.blockID, meta);
+        world.setBlockAndMetadata(x, y + 1, z, block.blockID, 8 | (meta & 1));
+    }
+
+    public static void placeDoorBlock(World world, int x, int y, int z, EnumDoor door) {
+        placeDoorBlock(world, x, y, z, 0, door == EnumDoor.IRON ? Block.doorSteel : Block.doorWood);
+    }
 }
