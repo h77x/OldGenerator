8fbcaa2edbe829874b220ed78eb8172c046e157c
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 21:52:54 +0100

    gen: add legacy 1.2.5 WeightedRandomChoice.java compatibility

---
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/WeightedRandomChoice.java
@@ -0,0 +1,2 @@
+package ca.spottedleaf.oldgenerator.generator.v125.structure.legacy;
+public class WeightedRandomChoice { protected int itemWeight; public WeightedRandomChoice(int w){this.itemWeight=w;} }
