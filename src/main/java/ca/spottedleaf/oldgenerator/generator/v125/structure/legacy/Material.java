918181eae5f4c12666bed2523c2fcbbc2a90bd5f
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 22:06:57 +0100

    gen: add vanilla Material.lava compatibility

--- src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/Material.java
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/Material.java
@@ -2,4 +2,5 @@ package ca.spottedleaf.oldgenerator.generator.v125.structure.legacy;
 public final class Material {
     private Material(){}
     public static final Block.BlockMaterial water = Block.waterStill.blockMaterial;
+    public static final Block.BlockMaterial lava = Block.lavaStill.blockMaterial;
 }
