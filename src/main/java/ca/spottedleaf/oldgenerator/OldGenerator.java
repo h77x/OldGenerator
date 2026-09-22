e257dfa2a077472e79d999d7790aed64ad4a4cf1
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 20:52:31 +0100

    Register Minecraft 1.2.5 world generator

--- src/main/java/ca/spottedleaf/oldgenerator/OldGenerator.java
+++ src/main/java/ca/spottedleaf/oldgenerator/OldGenerator.java
@@ -2,6 +2,7 @@ package ca.spottedleaf.oldgenerator;
 
 import ca.spottedleaf.oldgenerator.generator.b173.Beta173ChunkGenerator;
 import ca.spottedleaf.oldgenerator.generator.b173.listener.SkyGenerationListener;
+import ca.spottedleaf.oldgenerator.generator.v125.V125ChunkGenerator;
 import ca.spottedleaf.oldgenerator.listener.LegacyPopulateHack;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
@@ -15,7 +16,6 @@ public final class OldGenerator extends JavaPlugin {
 
     protected void setupMetrics() {
         final Metrics metrics = new Metrics(this, 7761);
-        // TODO custom charts?
     }
 
     @Override
@@ -30,13 +30,27 @@ public final class OldGenerator extends JavaPlugin {
 
     @Override
     public ChunkGenerator getDefaultWorldGenerator(final String worldName, final String id) {
-        if (id == null || (!id.equalsIgnoreCase("b173") && !id.equalsIgnoreCase("sb173"))) {
-            this.getLogger().warning("Id '" + id + "' is invalid, accepted ids: b173, sb173");
+        if (id == null) {
+            this.getLogger().warning("Generator id cannot be null. Accepted ids: b173, sb173, v125");
             return null;
         }
-        // TODO configurable
-        final boolean skylands = id.equalsIgnoreCase("sb173");
-        this.getLogger().info("Registering world generator for world '" + worldName + "' with configuration: {skylands:" + skylands + "}");
-        return new Beta173ChunkGenerator(skylands);
+
+        if (id.equalsIgnoreCase("b173")) {
+            this.getLogger().info("Registering Beta 1.7.3 overworld generator for '" + worldName + "'");
+            return new Beta173ChunkGenerator(false);
+        }
+
+        if (id.equalsIgnoreCase("sb173")) {
+            this.getLogger().info("Registering Beta 1.7.3 skylands generator for '" + worldName + "'");
+            return new Beta173ChunkGenerator(true);
+        }
+
+        if (id.equalsIgnoreCase("v125") || id.equalsIgnoreCase("1.2.5")) {
+            this.getLogger().info("Registering Minecraft 1.2.5 overworld generator for '" + worldName + "'");
+            return new V125ChunkGenerator();
+        }
+
+        this.getLogger().warning("Id '" + id + "' is invalid, accepted ids: b173, sb173, v125");
+        return null;
     }
 }
