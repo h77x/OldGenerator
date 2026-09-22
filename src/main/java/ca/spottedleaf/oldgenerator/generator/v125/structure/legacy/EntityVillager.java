fac42a36264701dbcc8d9871aad813161f28fe28
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 21:53:22 +0100

    gen: add legacy 1.2.5 EntityVillager.java compatibility

---
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/EntityVillager.java
@@ -0,0 +1,6 @@
+package ca.spottedleaf.oldgenerator.generator.v125.structure.legacy;
+public final class EntityVillager {
+    private final World world; private final int profession;
+    public EntityVillager(World w){this(w,0);} public EntityVillager(World w,int p){world=w;profession=p;}
+    public void setLocationAndAngles(double x,double y,double z,float yaw,float pitch){}
+}
