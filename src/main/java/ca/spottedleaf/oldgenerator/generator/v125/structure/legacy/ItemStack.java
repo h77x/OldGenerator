05e8bf68a6b2aed7a66b799897e51f03b1602776
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 21:52:59 +0100

    gen: add legacy 1.2.5 ItemStack.java compatibility

---
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/ItemStack.java
@@ -0,0 +1,2 @@
+package ca.spottedleaf.oldgenerator.generator.v125.structure.legacy;
+public final class ItemStack { public final int itemID,stackSize,itemDamage; public ItemStack(int id,int size,int damage){this.itemID=id;this.stackSize=size;this.itemDamage=damage;} }
