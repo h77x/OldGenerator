8bbcd3adf53c50c7509b8a774d5a48fd7c1b6f4d
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 22:09:56 +0100

    gen: implement vanilla legacy Material semantics

--- src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/Material.java
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/Material.java
@@ -1,6 +1,19 @@
 package ca.spottedleaf.oldgenerator.generator.v125.structure.legacy;
-public final class Material {
-    private Material(){}
-    public static final Block.BlockMaterial water = Block.waterStill.blockMaterial;
-    public static final Block.BlockMaterial lava = Block.lavaStill.blockMaterial;
+
+public class Material {
+    private final boolean solid;
+    private final boolean liquid;
+
+    public static final Material water = new Material(false, true);
+    public static final Material lava = new Material(false, true);
+
+    public Material(final boolean solid, final boolean liquid) {
+        this.solid = solid;
+        this.liquid = liquid;
+    }
+
+    public boolean isLiquid() { return this.liquid; }
+    public boolean isSolid() { return this.solid; }
+    public boolean blocksMovement() { return this.solid; }
+    public boolean isOpaque() { return this.solid; }
 }
