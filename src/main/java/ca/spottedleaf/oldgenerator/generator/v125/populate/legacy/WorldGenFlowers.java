7690546c491face23bce05a3734238824e0d9f88
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 22:14:07 +0100

    fix: remove obsolete BlockFlower dependency from WorldGenFlowers

--- src/main/java/ca/spottedleaf/oldgenerator/generator/v125/populate/legacy/WorldGenFlowers.java
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/populate/legacy/WorldGenFlowers.java
@@ -23,7 +23,7 @@ public class WorldGenFlowers extends WorldGenerator
             int var8 = par4 + par2Random.nextInt(4) - par2Random.nextInt(4);
             int var9 = par5 + par2Random.nextInt(8) - par2Random.nextInt(8);
 
-            if (par1World.isAirBlock(var7, var8, var9) && ((BlockFlower)Block.blocksList[this.plantBlockId]).canBlockStay(par1World, var7, var8, var9))
+            if (par1World.isAirBlock(var7, var8, var9) && Block.blocksList[this.plantBlockId].canBlockStay(par1World, var7, var8, var9))
             {
                 par1World.setBlock(var7, var8, var9, this.plantBlockId);
             }
