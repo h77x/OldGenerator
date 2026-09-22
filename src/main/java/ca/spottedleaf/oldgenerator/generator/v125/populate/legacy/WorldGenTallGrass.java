84eaae371ccde0b5ab3748aa31023f481ff07515
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 22:14:13 +0100

    fix: remove obsolete BlockFlower dependency from WorldGenTallGrass

--- src/main/java/ca/spottedleaf/oldgenerator/generator/v125/populate/legacy/WorldGenTallGrass.java
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/populate/legacy/WorldGenTallGrass.java
@@ -38,7 +38,7 @@ public class WorldGenTallGrass extends WorldGenerator
             int var9 = par4 + par2Random.nextInt(4) - par2Random.nextInt(4);
             int var10 = par5 + par2Random.nextInt(8) - par2Random.nextInt(8);
 
-            if (par1World.isAirBlock(var8, var9, var10) && ((BlockFlower)Block.blocksList[this.tallGrassID]).canBlockStay(par1World, var8, var9, var10))
+            if (par1World.isAirBlock(var8, var9, var10) && Block.blocksList[this.tallGrassID].canBlockStay(par1World, var8, var9, var10))
             {
                 par1World.setBlockAndMetadata(var8, var9, var10, this.tallGrassID, this.tallGrassMetadata);
             }
