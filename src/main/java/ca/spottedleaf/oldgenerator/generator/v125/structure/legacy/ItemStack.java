d3bcad65525e803d9ed30c671884939f84a38cf6
h77x <214206707+h77x@users.noreply.github.com>
2026-09-22 22:10:12 +0100

    gen: add vanilla 1.2.5 ItemStack constructors

--- src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/ItemStack.java
+++ src/main/java/ca/spottedleaf/oldgenerator/generator/v125/structure/legacy/ItemStack.java
@@ -1,2 +1,15 @@
 package ca.spottedleaf.oldgenerator.generator.v125.structure.legacy;
-public final class ItemStack { public final int itemID,stackSize,itemDamage; public ItemStack(int id,int size,int damage){this.itemID=id;this.stackSize=size;this.itemDamage=damage;} }
+
+public final class ItemStack {
+    public final int itemID, stackSize, itemDamage;
+    public ItemStack(final Item item) { this(item, 1, 0); }
+    public ItemStack(final Item item, final int size) { this(item, size, 0); }
+    public ItemStack(final Item item, final int size, final int damage) {
+        this(item == null ? -1 : item.shiftedIndex, size, damage);
+    }
+    public ItemStack(final int id, final int size, final int damage) {
+        this.itemID = id;
+        this.stackSize = size;
+        this.itemDamage = damage;
+    }
+}
