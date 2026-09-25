4668d8dd6f4d90724a48c41b612c49c08e75421c
h77x <214206707+h77x@users.noreply.github.com>
2026-09-25 22:11:55 +0100

    fix: guard LimitedRegion reads and height lookups

--- src/main/java/ca/spottedleaf/oldgenerator/world/LimitedRegionBlockAccess.java
+++ src/main/java/ca/spottedleaf/oldgenerator/world/LimitedRegionBlockAccess.java
@@ -26,18 +26,64 @@ public final class LimitedRegionBlockAccess implements BlockAccess {
                 && this.region.isInRegion(minX + 15, this.minHeight, minZ + 15);
     }
 
-    @Override public Material getType(final int x, final int y, final int z) { return this.region.getType(x, y, z); }
-    @Override public void setType(final int x, final int y, final int z, final Material material) { if (this.region.isInRegion(x,y,z)) this.region.setType(x,y,z,material); }
-    @Override public void setType(final int x, final int y, final int z, final Material material, final boolean applyPhysics) { if (this.region.isInRegion(x,y,z)) this.region.setType(x,y,z,material); }
-    @Override public BlockData getBlockData(final int x, final int y, final int z) { return this.region.getBlockData(x,y,z); }
-    @Override public void setBlockData(final int x, final int y, final int z, final BlockData data) { if (this.region.isInRegion(x,y,z)) this.region.setBlockData(x,y,z,data); }
-    @Override public void setBlockData(final int x, final int y, final int z, final BlockData data, final boolean applyPhysics) { if (this.region.isInRegion(x,y,z)) this.region.setBlockData(x,y,z,data); }
-    @Override public BlockState getBlockState(final int x, final int y, final int z) { return this.region.getBlockState(x,y,z); }
+    @Override
+    public boolean isInRegion(final int x, final int y, final int z) {
+        return y >= this.minHeight && y <= this.maxHeight && this.region.isInRegion(x, y, z);
+    }
+
+    @Override
+    public Material getType(final int x, final int y, final int z) {
+        return this.isInRegion(x, y, z) ? this.region.getType(x, y, z) : Material.AIR;
+    }
+
+    @Override
+    public void setType(final int x, final int y, final int z, final Material material) {
+        if (this.isInRegion(x, y, z)) this.region.setType(x, y, z, material);
+    }
+
+    @Override
+    public void setType(final int x, final int y, final int z, final Material material, final boolean applyPhysics) {
+        if (this.isInRegion(x, y, z)) this.region.setType(x, y, z, material);
+    }
+
+    @Override
+    public BlockData getBlockData(final int x, final int y, final int z) {
+        return this.isInRegion(x, y, z) ? this.region.getBlockData(x, y, z) : Material.AIR.createBlockData();
+    }
+
+    @Override
+    public void setBlockData(final int x, final int y, final int z, final BlockData data) {
+        if (this.isInRegion(x, y, z)) this.region.setBlockData(x, y, z, data);
+    }
+
+    @Override
+    public void setBlockData(final int x, final int y, final int z, final BlockData data, final boolean applyPhysics) {
+        if (this.isInRegion(x, y, z)) this.region.setBlockData(x, y, z, data);
+    }
+
+    @Override
+    public BlockState getBlockState(final int x, final int y, final int z) {
+        return this.isInRegion(x, y, z) ? this.region.getBlockState(x, y, z) : null;
+    }
+
     @Override public byte getLightFromSky(final int x, final int y, final int z) { return 0; }
     @Override public byte getLightFromBlocks(final int x, final int y, final int z) { return 0; }
     @Override public byte getLightLevel(final int x, final int y, final int z) { return 0; }
-    @Override public int getHighestBlockYAt(final int x, final int z) { return this.region.getHighestBlockYAt(x,z); }
-    @Override public int getHighestBlockYAt(final int x, final int z, final HeightMap map) { return this.region.getHighestBlockYAt(x,z,map); }
+
+    @Override
+    public int getHighestBlockYAt(final int x, final int z) {
+        return this.isInRegion(x, this.minHeight, z)
+                ? this.region.getHighestBlockYAt(x, z)
+                : this.minHeight;
+    }
+
+    @Override
+    public int getHighestBlockYAt(final int x, final int z, final HeightMap map) {
+        return this.isInRegion(x, this.minHeight, z)
+                ? this.region.getHighestBlockYAt(x, z, map)
+                : this.minHeight;
+    }
+
     @Override public int getMinHeight() { return this.minHeight; }
     @Override public int getMaxHeight() { return this.maxHeight; }
 }
